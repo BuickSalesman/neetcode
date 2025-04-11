9cf2c56c6c4086e0bcebad8208b10ef796ac8a56
BuickSalesman <adam.odonnell.e@gmail.com>
2025-04-11 15:59:58 -0400

    solved maximum subarray

---
+++ maximumSubarray.js
@@ -0,0 +1,20 @@
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @return {number}
+   */
+  maxSubArray(nums) {
+    let maxSub = nums[0];
+    let curSum = 0;
+
+    for (const n of nums) {
+      if (curSum < 0) {
+        curSum = 0;
+      }
+      curSum += n;
+      maxSub = Math.max(maxSub, curSum);
+    }
+
+    return maxSub;
+  }
+}
